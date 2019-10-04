5c686cc1f46e21bd58672cab7ae1787903a26195
Tony He <tony.he2@wework.com>
2019-10-04 16:16:37 +0800

    start up setting up

--- WeatherDarkSky/AppDelegate.swift
+++ WeatherDarkSky/AppDelegate.swift
@@ -10,11 +10,17 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow.init()
+        let homePageVC = HomePageListViewController()
+        window?.rootViewController = UINavigationController.init(rootViewController: homePageVC)
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
---
+++ WeatherDarkSky/HomePage/HomePageListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  WeatherDarkSky
+//
+//  Created by Tony He on 2019/10/4.
+//  Copyright © 2019 Tony He. All rights reserved.
+//
+
+import UIKit
+
+class HomePageListViewController: UIViewController {
+
+    let tableView:UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .white
+        
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        buildUI()
+    }
+
+    private func buildUI(){
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+}
+
+extension HomePageListViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
